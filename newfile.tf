10f8a6708c32662cc857fcff86fa7eae6c80af96
kthollgh <126017932+kthollgh@users.noreply.github.com>
2023-02-21 14:24:02 -0600

    Update newfile.tf

--- newfile.tf
+++ newfile.tf
@@ -1,16 +1,21 @@
-variable "subnet_ids" {
-  type = list(string)
+# The default "aws" configuration is used for AWS resources in the root
+# module where no explicit provider instance is selected.
+provider "aws" {
+  region = "us-west-1"
 }
 
-resource "aws_instance" "server" {
-  # Create one instance for each subnet
-  count = length(var.subnet_ids)
+# An alternate configuration is also defined for a different
+# region, using the alias "usw2".
+provider "aws" {
+  alias  = "usw2"
+  region = "us-west-2"
+}
 
-  ami           = "ami-a1b2c3d4"
-  instance_type = "t2.micro"
-  subnet_id     = var.subnet_ids[count.index]
-
-  tags = {
-    Name = "Server ${count.index}"
+# An example child module is instantiated with the alternate configuration,
+# so any AWS resources it defines will use the us-west-2 region.
+module "example" {
+  source    = "./example"
+  providers = {
+    aws = aws.usw2
   }
 }
