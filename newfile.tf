dd3c38561dd3d2a2355991f196687691bf2a2a5f
kthollgh <126017932+kthollgh@users.noreply.github.com>
2023-02-21 14:18:02 -0600

    Update newfile.tf

--- newfile.tf
+++ newfile.tf
@@ -1,8 +1,14 @@
+variable "subnet_ids" {
+  type = list(string)
+}
+
 resource "aws_instance" "server" {
-  count = 4 # create four similar EC2 instances
+  # Create one instance for each subnet
+  count = length(var.subnet_ids)
 
   ami           = "ami-a1b2c3d4"
   instance_type = "t2.micro"
+  subnet_id     = var.subnet_ids[count.index]
 
   tags = {
     Name = "Server ${count.index}"
