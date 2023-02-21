57c59ec9e97a8dad5a682afad8f98c7206a47a43
kthollgh <126017932+kthollgh@users.noreply.github.com>
2023-02-21 14:06:31 -0600

    Update newfile.tf

--- newfile.tf
+++ newfile.tf
@@ -1,7 +1,8 @@
-{
-  "variable": {
-    "example": {
-      "default": "hello"
-    }
-  }
+variable "example" {
+  default = "hello"
+}
+
+resource "aws_instance" "example" {
+  instance_type = "t2.micro"
+  ami           = "ami-abc123"
 }
