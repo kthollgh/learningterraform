eb46116ee9c2af2aee9814e39fbebb5ebdac8a3b
kthollgh <126017932+kthollgh@users.noreply.github.com>
2023-02-21 14:16:56 -0600

    Update newfile.tf

--- newfile.tf
+++ newfile.tf
@@ -1,1 +1,10 @@
-{testing tf block}
+resource "aws_instance" "server" {
+  count = 4 # create four similar EC2 instances
+
+  ami           = "ami-a1b2c3d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Server ${count.index}"
+  }
+}
